c6280888b841f9912bb022c202c4b7fb4ca651b7
Alicia <aliciaortega1986@gmail.com>
2025-05-09 13:38:41 +0200

    Estructura del microservicio Cursos pendiente de completar con nuevos commints

---
+++ src/main/java/com/springcloud/msvc/cursos/SpringCloudPostgreSqlDockerKubernetesCursosApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.msvc.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudPostgreSqlDockerKubernetesCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudPostgreSqlDockerKubernetesCursosApplication.class, args);
+	}
+
+}
